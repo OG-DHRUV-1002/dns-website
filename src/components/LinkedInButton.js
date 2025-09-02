198550abc50732217891ea006b00acd282f0a969
Dhruv Hadal <dhruvhadal@gmail.com>
2025-09-02 09:24:27 +0530

    LinkedInButton.js

--- src/components/LinkedInButton.js
+++ src/components/LinkedInButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LinkedInButton = () => {
   // IMPORTANT: Replace with your actual LinkedIn profile URL
-  const linkedInProfileUrl = "https://www.linkedin.com";
+  const linkedInProfileUrl = "https://www.linkedin.com/company/neelvardhan/";
 
   return (
     <a href={linkedInProfileUrl} target="_blank" rel="noopener noreferrer" className="linkedin-button">
