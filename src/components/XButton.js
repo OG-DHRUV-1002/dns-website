789760a36fb422e2cd82aa53a68a082255f3c3e5
Dhruv Hadal <dhruvhadal@gmail.com>
2025-09-02 08:35:53 +0530

    XButton.js

---
+++ src/components/XButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './SocialButtons.css'; // Use a shared CSS file for styling
+
+const XButton = () => {
+  // Replace with your club's actual X profile URL
+  const xProfileUrl = 'https://twitter.com/YourClubHandle';
+
+  return (
+    <a href={xProfileUrl} target="_blank" rel="noopener noreferrer" className="social-button x-button">
+      Connect on X
+    </a>
+  );
+};
+
+export default XButton;
