306ea701397a7e76673401f9d120780c287b4366
Dhruv Hadal <dhruvhadal@gmail.com>
2025-09-13 11:54:15 +0530

    EventCard.js

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -7,8 +7,8 @@ const EventCard = ({ event }) => {
       <h3>{event.title}</h3>
       <p><strong>In partnership with:</strong> {event.ngo}</p>
       <p><strong>📅 Date:</strong> {event.date}</p>
-      <p><strong>🕖 Time:</strong> {event.date}</p>
-      <p><strong>📍 Location:</strong> {event.date}</p>
+      <p><strong>🕖 Time:</strong> {event.time}</p>
+      <p><strong>📍 Location:</strong> {event.location}</p>
       <p>{event.description}</p>
       <a href={event.link} target="_blank" rel="noopener noreferrer">Sign Up Here</a>
     </div>
