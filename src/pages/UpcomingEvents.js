c746f01ce66bb6f3eb9d1579fd9d752ece4a1bf2
Dhruv Hadal <dhruvhadal@gmail.com>
2025-09-13 17:27:10 +0530

    UpcomingEvents.js

--- src/pages/UpcomingEvents.js
+++ src/pages/UpcomingEvents.js
@@ -1,5 +1,5 @@
 // src/pages/UpcomingEvents.js
-import React from 'react';
+import React, { useState } from 'react'; // <-- IMPORT useState HERE
 import EventCard from '../components/EventCard';
 
 const UpcomingEvents = () => {
