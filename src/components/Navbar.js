0dec42587469838f8a1e9f82a34e72cd1646ea83
Dhruv Hadal <dhruvhadal@gmail.com>
2025-09-14 11:20:04 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,9 +20,8 @@ const Navbar = () => {
     setIsMenuOpen(false); // Close mobile menu if open
 
     const confirmNavigation = window.confirm(
-  "IMPORTANT: Please ensure you are a registered member of our WhatsApp community BEFORE you sign up for an event. Only sign-ups from registered members will be considered valid.\n\n" +
-  "Also, make sure you are logged into your ConnectFor account.\n\n" +
-  "Click 'OK' to proceed to the Upcoming Events page. Otherwise, click 'Cancel'."
+  "IMPORTANT: Please ensure you are a registered member of our WhatsApp community BEFORE you sign up for an event. Only sign-ups from registered members will be considered valid.\n" +
+  "Also, make sure you are logged into your ConnectFor account."
 );
 
     if (confirmNavigation) {
