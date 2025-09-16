6361af9724ed3f5651dab5e4d4f1ebbc34b890e8
Dhruv Hadal <dhruvhadal@gmail.com>
2025-09-17 00:46:37 +0530

    Update UpcomingEvents.js

--- src/pages/UpcomingEvents.js
+++ src/pages/UpcomingEvents.js
@@ -14,7 +14,7 @@ const UpcomingEvents = () => {
       time: "7:00 AM – 9:00 AM",
       location: "Juhu Koliwada Beach, Santacruz(West)",
       description: 'This International Coastal Cleanup Day, let’s come together to create a real impact where it matters most — our oceans. Bhumi, in collaboration with VFS Global, is organizing large-scale coastal cleanups in Mumbai, and we are inviting corporates, Individuals, Colleges to come along.',
-      link: 'https://www.connectfor.org/volunteer-now?aid=1574953346033&eventId=59638' // Replace with actual event link
+      link: 'https://forms.gle/Hn2qxrHLSfVGUKk49' // Replace with actual event link
     },
     {
       title: 'Carter Beach Road Cleanup',
