e1a047f90b1cbcc6b03d9abfdd008982641d73a6
Dhruv Hadal <dhruvhadal@gmail.com>
2025-09-19 00:47:22 +0530

    Update UpcomingEvents.js

--- src/pages/UpcomingEvents.js
+++ src/pages/UpcomingEvents.js
@@ -8,11 +8,11 @@ const UpcomingEvents = () => {
   // This data can be fetched from a server in the future
   const events = [
     {
-      title: 'Juhu Koliwada Beach Cleanup Volunteers',
+      title: 'Juhu Silver Beach Cleanup Volunteers',
       ngo: 'Bhumi with VFS Global',
       date: 'Saturday, September 20, 2025',
-      time: "7:00 AM – 9:00 AM",
-      location: "Juhu Koliwada Beach, Santacruz(West)",
+      time: "7:30 AM – 9:00 AM",
+      location: "Silver Beach, Juhu-Mumbai",
       description: 'This International Coastal Cleanup Day, let’s come together to create a real impact where it matters most — our oceans. Bhumi, in collaboration with VFS Global, is organizing large-scale coastal cleanups in Mumbai, and we are inviting corporates, Individuals, Colleges to come along.',
       link: 'https://forms.gle/Hn2qxrHLSfVGUKk49' // Replace with actual event link
     },
